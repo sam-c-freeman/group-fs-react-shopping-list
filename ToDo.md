7a93b9d593f5df908916aec13b93a1c381e1b807
Sam Freeman <samcfreeman888@gmail.com>
2022-09-15 16:58:34 -0500

    set up where components and code will go.  Started assigning possible tasks in the ToDo.md document

---
+++ ToDo.md
@@ -0,0 +1,7 @@
+# Tasks for Friday/How to Divide Things
+
+* Sarah and Sam: Item List Component (GET, DeleteID, PutID routes)
+* Dan: Basket
+* Yani: Form
+* 
+* 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,53 @@
 import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
+import ItemForm from '../ItemForm/ItemForm.jsx'
+import Basket from '../Basket/Basket.jsx'
+import ItemList from '../ItemList/ItemList.jsx'
 import './App.css';
+//if anything doesn't work check above for typos ^^^
+
+
 
 
 function App() {
+    
+    let [itemList, setItemList] = useState([]);
+
+    useEffect(() => {
+        fetchItems();
+    }, [])
+
+
+    //GET request function
+
+    //fetchItems();  
+
+
+
+    //POST  request function
+
+    //addItem();
+
+
+
+    //DELETE (all) function
+
+    //PUT (all) to reset list
+
+    //DELETE/ID individual item function to pass to ItemList
+
+    //PUT/ID to update individual items 
+
+    
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                {/* <ItemForm props={addItem} />
+                <Basket props={itemList, resetAll, deleteAll } />
+                <ItemList props={itemList, put/ID, delete/ID} /> */} 
             </main>
         </div>
     );
